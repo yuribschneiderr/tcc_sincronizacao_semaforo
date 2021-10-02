f430806b1c0a9a2f4f88c71d6958182de21a8cce
kleyton <kleytonmatosramos@gmai.com>
2021-10-02 17:41:35 -0300

    feat: adiciona a rotina de stage

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 # import pandas
 import pandas as pd
 import numpy as np
+import time
 
 data = pd.read_csv('./data/sincro.csv', sep = ';', encoding='latin-1')
 
-print(data.query('dia_semana == 35'))
-
-# mocks
-# TODO
-# Criar mocks para os dados abaixo
-
+# velocidade media sensor
+velocidade = 'velocidade aleatoria'
 dia = 'dia aleatorio'
-hora = 'hora aleatoria'
 
-# TODO
-#def get_stage
-#    if hora >= 12
-#        stage_1(dia, hora)
-#
-#    if hora < 12
-#        stage_2(dia, hora)
+def stage(velocidade, dia):
+    # define intervalo de velocidade para horario de pico
+    intervalo = list(range(41))
 
-#def stage_1(dia, hora)
-#    velocidade = data.query('dia_semana == $dia and hora_dia == $hora')
-#
-#    velocidade = range(35..60)
-#
-#    calc_horaria_velocidade(0, velocidade, 1)
+    # retorna hora e dia da semana baseado na velocidade
+    consulta = data.query('v_media == "{}" & dia_semana == "{}"'.format(velocidade, dia))
 
-#def stage_2(dia, hora)
-#    velocidade = data.query('dia_semana == dia and hora_dia == hora')
-#
-#    calc_horaria_velocidade(0, velocidade, 1)
+    print("\n Dia: {}, hora: {}".format(consulta.dia_semana.item(), consulta.hora_dia.item()))
+
+    # define paramentros para calculo de temporização
+    if velocidade in intervalo:
+        calc_horaria_velocidade(0, (max(intervalo)/3.6), 1)
+    else:
+        calc_horaria_velocidade(0, (60/3.6), 1)
 
 def calc_horaria_velocidade(velocidade_inicial, velocidade_final, aceleracao):
     # tempo até o veiculo atigir a velocidade maxima
@@ -41,7 +32,7 @@ def calc_horaria_velocidade(velocidade_inicial, velocidade_final, aceleracao):
     calc_horaria_posicao(velocidade_inicial, velocidade_final, aceleracao, tempo)
 
 def calc_horaria_posicao(velocidade_inicial, velocidade_final, aceleracao, tempo):
-    # Distancia até o veiculo atingir a velocidade maxima
+    # distancia até o veiculo atingir a velocidade maxima
     delta_s0 = velocidade_inicial * tempo + (aceleracao * tempo**2/2)
 
     # calculo do tempo restante para a primeira meta de abertura
@@ -60,10 +51,11 @@ def calc_movimento_uniforme_s1(delta_s0, velocidade_final, tempo):
     # tempo restante até o veiculo atingir a primeira meta de abertura.
     delta_t = delta_s1/velocidade_final
 
+
     # tempo total para abertura do primeiro semáforo.
     resultado_s1 = tempo + delta_t
 
-    print("Resultado tempo 1: {0:f}".format(resultado_s1))
+    print("\n Resultado tempo 1: {0:f}".format(resultado_s1))
 
 def calc_movimento_uniforme_s2(velocidade_final):
     # distancia (em metros) da meta de abertura de s1 até a meta de abertura de s2
@@ -72,6 +64,4 @@ def calc_movimento_uniforme_s2(velocidade_final):
     # tempo restante até o veiculo atingir a segunda meta de abertura a partir da primeira meta
     tempo = distancia/velocidade_final
 
-    print("Resultado tempo 2: {0:f}".format(tempo))
-
-#calc_horaria_velocidade(0, velocidade, 1)
+    print("\n Resultado tempo 2: {0:f}".format(tempo))
